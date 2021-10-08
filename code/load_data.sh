f5b57cad96dce0bd24560bf947001a04a145e9cd
mel-oezkan <meloezkan@uni-osnabrueck.de>
2021-10-08 18:30:31 +0200

    revert changes
    Revert changes made to  the load_data file

--- code/load_data.sh
+++ code/load_data.sh
@@ -4,8 +4,6 @@
 mkdir -p data/raw/
 
 # download the three csv files
-echo "Dowloading Data"
-python -m wget -o data/raw/data_analysis.csv https://myshare.uni-osnabrueck.de/f/3e5276caf72b46e7ace2/?dl=1
-python -m wget -o data/raw/data_science.csv https://myshare.uni-osnabrueck.de/f/e620aff7719948d18a52/?dl=1
-python -m wget -o data/raw/data_visualization.csv https://myshare.uni-osnabrueck.de/f/9ddaab064c68483e9bff/?dl=1
-
+wget -nv https://myshare.uni-osnabrueck.de/f/3e5276caf72b46e7ace2/?dl=1 -O data/raw/data_analysis.csv
+wget -nv https://myshare.uni-osnabrueck.de/f/e620aff7719948d18a52/?dl=1 -O data/raw/data_science.csv
+wget -nv https://myshare.uni-osnabrueck.de/f/9ddaab064c68483e9bff/?dl=1 -O data/raw/data_visualization.csv
