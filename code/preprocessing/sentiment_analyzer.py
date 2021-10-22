b271a3f62cddc90a56bcaa13b9f27e49cbf7b5a5
mel-oezkan <meloezkan@uni-osnabrueck.de>
2021-10-22 18:08:20 +0200

    implemented sentiment analyzer class

---
+++ code/preprocessing/sentiment_analyzer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Preprocessor that removes punctuation from the original tweet text.
+
+Created on Wed Sep 29 09:45:56 2021
+
+@author: mel-oezkan
+"""
+
+from code.preprocessing.preprocessor import Preprocessor
+from code.util import COLUMN_TWEET, COLUMN_SENTIMENT
+
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+# adds the sentiment of the tweet using a pretrained model
+# of the nltk library
+
+
+class SentimentAnalyzer(Preprocessor):
+
+    # constructor
+    def __init__(self):
+        # input column "tweet", new output column
+        super().__init__([COLUMN_TWEET], COLUMN_SENTIMENT)
+
+    # set internal variables based on input columns
+    def _set_variables(self, inputs):
+        # store punctuation for later reference
+        self.analyzer = SentimentIntensityAnalyzer()
+
+    # get preprocessed column based on data frame and internal variables
+    def _get_values(self, inputs):
+        """Get the polar sentiment of the tweets"""
+
+        polar_sentiment = []
+
+        for tweet in inputs[0]:
+            polar_sentiment.append(self.analyzer(tweet))
+
+        return polar_sentiment
