c0dfedf0e9b8664ede507d2505f95d263e2aef30
mel-oezkan <meloezkan@uni-osnabrueck.de>
2021-10-26 22:12:37 +0200

    rewriting set_variable du to lack of data

--- code/feature_extraction/language_feature.py
+++ code/feature_extraction/language_feature.py
@@ -6,7 +6,10 @@ Creates one hot encodings for the different languages
 @author: meloezkan
 """
 
+from os import path
 import numpy as np
+import pandas as pd
+import pathlib
 from sklearn.preprocessing import OneHotEncoder
 
 from code.feature_extraction.feature_extractor import FeatureExtractor
@@ -17,13 +20,28 @@ from code.feature_extraction.feature_extractor import FeatureExtractor
 class LanguageFeature(FeatureExtractor):
 
     # constructor
-    def __init__(self):
+    def __init__(self, source_file):
         super().__init__(['language'], "language_encoding")
 
+        self.source = source_file
+        self.source_file = pathlib.Path(self.source_file)
     # takes the inputs and fetches the unique country count
+
     def _set_variables(self, inputs):
 
-        uniqe_elems = set(inputs)
+        # check the source file befor using
+        assert self.source_file.exists(), (
+            "Given Source file does not exits")
+        assert self.source_file.suffix == "csv", (
+            "Given source file has to be a csv")
+
+        # read the file and fetch the language column
+        data = pd.read_csv(self.source_file.name)
+        assert "language" in data.columns, (
+            "Given source file has to contain a language column")
+        country_codes = data["lanugages"]
+
+        uniqe_elems = set(country_codes)
         self.oneHot = OneHotEncoder(sparse=False)
 
         # create the embeding scheme by fitting
@@ -31,4 +49,6 @@ class LanguageFeature(FeatureExtractor):
 
     def _get_values(self, inputs):
         # create the encodings by applying transformation
-        return self.oneHot.transform(inputs)
+
+        conv_inputs = [[value] for value in inputs[0]]
+        return self.oneHot.transform(conv_inputs)
