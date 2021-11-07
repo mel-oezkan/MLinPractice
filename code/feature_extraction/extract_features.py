ea0b52a34d44933339abe3194e036b591ac3e165
mel-oezkan <meloezkan@uni-osnabrueck.de>
2021-11-07 17:37:49 +0100

    added feature and commented some stuff

--- code/feature_extraction/extract_features.py
+++ code/feature_extraction/extract_features.py
@@ -8,25 +8,43 @@ Created on Wed Sep 29 11:00:24 2021
 @author: lbechberger
 """
 
-import argparse, csv, pickle
+import argparse
+import csv
+import pickle
 import pandas as pd
 import numpy as np
 from code.feature_extraction.character_length import CharacterLength
 from code.feature_extraction.feature_collector import FeatureCollector
+from code.feature_extraction.language_feature import LanguageFeature
 from code.util import COLUMN_TWEET, COLUMN_LABEL
 
 
 # setting up CLI
-parser = argparse.ArgumentParser(description = "Feature Extraction")
-parser.add_argument("input_file", help = "path to the input csv file")
-parser.add_argument("output_file", help = "path to the output pickle file")
-parser.add_argument("-e", "--export_file", help = "create a pipeline and export to the given location", default = None)
-parser.add_argument("-i", "--import_file", help = "import an existing pipeline from the given location", default = None)
-parser.add_argument("-c", "--char_length", action = "store_true", help = "compute the number of characters in the tweet")
+parser = argparse.ArgumentParser(description="Feature Extraction")
+parser.add_argument("input_file", help="path to the input csv file")
+parser.add_argument("output_file", help="path to the output pickle file")
+parser.add_argument("-e", "--export_file",
+                    help="create a pipeline and export to the given location", default=None)
+parser.add_argument("-i", "--import_file",
+                    help="import an existing pipeline from the given location", default=None)
+
+# Character len feature arguments
+parser.add_argument("-c", "--char_length", action="store_true",
+                    help="compute the number of characters in the tweet")
+parser.add_argument("--column_name", action="store_true",
+                    help="compute the number of characters in the tweet")
+
+# Language Feature arguments
+parser.add_argument("-l", "--language_feature", action="store_true",
+                    help="convert the language of the tweet iinto a one hot encoding")
+parser.add_argument("--language_source", action="store_true", default=None,
+                    help="convert the language of the tweet iinto a one hot encoding")
+
 args = parser.parse_args()
 
 # load data
-df = pd.read_csv(args.input_file, quoting = csv.QUOTE_NONNUMERIC, lineterminator = "\n")
+df = pd.read_csv(args.input_file, quoting=csv.QUOTE_NONNUMERIC,
+                 lineterminator="\n")
 
 if args.import_file is not None:
     # simply import an exisiting FeatureCollector
@@ -40,10 +58,12 @@ else:    # need to create FeatureCollector manually
     if args.char_length:
         # character length of original tweet (without any changes)
         features.append(CharacterLength(COLUMN_TWEET))
-    
+    if args.language_feature:
+        features.append(LanguageFeature())
+
     # create overall FeatureCollector
     feature_collector = FeatureCollector(features)
-    
+
     # fit it on the given data set (assumed to be training data)
     feature_collector.fit(df)
 
@@ -57,7 +77,7 @@ label_array = np.array(df[COLUMN_LABEL])
 label_array = label_array.reshape(-1, 1)
 
 # store the results
-results = {"features": feature_array, "labels": label_array, 
+results = {"features": feature_array, "labels": label_array,
            "feature_names": feature_collector.get_feature_names()}
 with open(args.output_file, 'wb') as f_out:
     pickle.dump(results, f_out)
