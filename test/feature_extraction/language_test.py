c900a61de1b8faf93bd9f05cb4c5854b30a3c744
mel-oezkan <meloezkan@uni-osnabrueck.de>
2021-11-08 15:13:11 +0100

    language feature test draft

--- test/feature_extraction/language_test.py
+++ test/feature_extraction/language_test.py
@@ -8,30 +8,52 @@ Created on Thu Oct  7 14:51:00 2021
 
 import unittest
 import pandas as pd
-import nltk
-from code.feature_extraction.bigrams import BigramFeature
+import tempfile
+
+from sklearn.preprocessing import OneHotEncoder
+
+from code.feature_extraction.language_feature import LanguageFeature
 
 
 class LanguageTest(unittest.TestCase):
+    """ Testing the language feature. To create the encodings
+    of the languages we need a sourcefile. For that using a temp
+    file shoud suffice 
+    """
 
     def setUp(self):
-        self.INPUT_COLUMN = "input"
-        self.language_feature = None
+        self.INPUT_COLUMN = 'language'
+        self.data = ['en', "de", "fr", "sp"]
+
         self.df = pd.DataFrame()
-        self.df[self.INPUT_COLUMN] = [
-            'en', "de", "fr", "sp"
-        ]
+        self.df[self.INPUT_COLUMN] = self.data
 
-    def test_input_columns(self):
-        self.assertEqual()
+        self.temp_file = tempfile.NamedTemporaryFile(suffix=".csv")
+        self.df.to_csv(self.temp_file.name)
 
-    def test_feature_name(self):
-        self.assertEqual(self.language_feature.get_feature_name(),
-                         self.INPUT_COLUMN + "_bigrams")
+        self.language_feature = LanguageFeature(self.temp_file)
 
-    def test_created_encodings(self):
-        self.language_feature.fit(self.df)
-        # modify to fit the requirements
+    def test_column_name(self):
+
+        # create the encodings using sklearn
+        oneHot = OneHotEncoder(sparse=False)
+        oneHot.fit([[x] for x in self.data])
+
+        base_encodings = oneHot.transform(self.data)
+
+        # create encodings with feature class
+        feature_encodings = self.language_feature.fit_transform(self.df)
+
+        print(base_encodings)
+        print(feature_encodings)
+
+        self.assertEqual(base_encodings, feature_encodings)
+
+    def test_encodings(self):
+        """Chek if the generated encodings equalt the 
+        onece which should have been generated """
+
+        pass
 
 
 if __name__ == '__main__':
