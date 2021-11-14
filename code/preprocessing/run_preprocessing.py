c06c1d7209741fdbdcd47fa01cb1d853b72f4b62
Melih <m.oezkan@gerhard-schubert.de>
2021-11-14 23:25:32 +0100

    added link remover args

--- code/preprocessing/run_preprocessing.py
+++ code/preprocessing/run_preprocessing.py
@@ -11,17 +11,23 @@ Created on Tue Sep 28 16:43:18 2021
 import argparse, csv, pickle
 import pandas as pd
 from sklearn.pipeline import make_pipeline
+from code.preprocessing.link_remover import LinkRemover
 from code.preprocessing.punctuation_remover import PunctuationRemover
+from code.preprocessing.stopword_remover import StopWords
 from code.preprocessing.tokenizer import Tokenizer
-from code.util import COLUMN_TWEET, SUFFIX_TOKENIZED
+from code.util import COLUMN_LINKS, COLUMN_PUNCTUATION, COLUMN_TWEET, SUFFIX_TOKENIZED
 
 # setting up CLI
 parser = argparse.ArgumentParser(description = "Various preprocessing steps")
 parser.add_argument("input_file", help = "path to the input csv file")
 parser.add_argument("output_file", help = "path to the output csv file")
+
 parser.add_argument("-p", "--punctuation", action = "store_true", help = "remove punctuation")
+parser.add_argument("-l", "--links", action = "store_true", help = "removes links in the tweet")
 parser.add_argument("-t", "--tokenize", action = "store_true", help = "tokenize given column into individual words")
 parser.add_argument("--tokenize_input", help = "input column to tokenize", default = COLUMN_TWEET)
+parser.add_argument("-s", "--stopword", action = "store_true", help = "removes stopwords given tokenized inputs")
+
 parser.add_argument("-e", "--export_file", help = "create a pipeline and export to the given location", default = None)
 args = parser.parse_args()
 
@@ -32,8 +38,15 @@ df = pd.read_csv(args.input_file, quoting = csv.QUOTE_NONNUMERIC, lineterminator
 preprocessors = []
 if args.punctuation:
     preprocessors.append(PunctuationRemover())
+if args.links:
+    preprocessors.append(LinkRemover(COLUMN_PUNCTUATION, COLUMN_LINKS))
 if args.tokenize:
     preprocessors.append(Tokenizer(args.tokenize_input, args.tokenize_input + SUFFIX_TOKENIZED))
+    
+    if args.stopword:
+        # depends on the tokenized inputs 
+        preprocessors.append(StopWords(args.tokenize_input + SUFFIX_TOKENIZED))
+
 
 # call all preprocessing steps
 for preprocessor in preprocessors:
