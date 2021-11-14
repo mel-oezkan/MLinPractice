1e9f0d997ab9caacbc50c02e638952e2148c98cc
Melih <m.oezkan@gerhard-schubert.de>
2021-11-14 20:12:47 +0100

    update stopword class

--- code/preprocessing/stopword_remover.py
+++ code/preprocessing/stopword_remover.py
@@ -11,12 +11,15 @@ import ast
 from code.preprocessing.preprocessor import Preprocessor
 from nltk.corpus import stopwords
 
+from code.util import COLUMN_STOPWORDS
+
+
 class StopWords(Preprocessor):
     """Remove stop words from the tokenized words."""
     
-    def __init__(self, input_column, output_column):
+    def __init__(self, input_column):
         """Initialize the Tokenizer with the given input and output column."""
-        super().__init__([input_column], output_column)
+        super().__init__([input_column], COLUMN_STOPWORDS)
     
     # don't need to implement _set_variables(), since no variables to set
     
@@ -27,6 +30,11 @@ class StopWords(Preprocessor):
 
         # iterate over the pandas Series        
         for tweet in inputs[0]:
+
+            # make sure tweet is tokenized
+            assert tweet.startswith('[')
+            assert tweet.endswith(']')
+
             tweet_token = ast.literal_eval(tweet)
             current_tweet = [token for token in tweet_token if token not in stopwords]
             
